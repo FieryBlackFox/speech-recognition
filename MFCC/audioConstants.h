fb8b4f901980ec4f845a3f0bf023bf4d84427a93
FieryBlackFox <ya.daria-belozerova@yandex.ru>
2017-04-22 19:19:53 +0300

    Create audioConstants.h

---
+++ MFCC/audioConstants.h
@@ -0,0 +1,9 @@
+#ifndef AUDIOCONSTANTS_H_
+#define AUDIOCONSTANTS_H_
+
+const int FRECUENCY_MIN = 100;
+const int FRECUENCY_MAX = 4000;
+
+const int MFCC_COUNT = 13;
+
+#endif /* AUDIOCONSTANTS_H_ */
